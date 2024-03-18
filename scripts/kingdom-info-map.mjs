f4f58de8c8afba9e5248afaa9027f189f29edc8d
Andrew Stockman <andrew.stockman@ihsmarkit.com>
2024-03-18 10:24:10 -0600

    initial commit

---
+++ scripts/kingdom-info-map.mjs
@@ -0,0 +1,114 @@
+import KingdomInfoLayer from "./kingdom-info-layer.mjs";
+
+export default class KingdomInfoMap {
+    constructor() {
+    }
+
+  /**
+   * The Scene ID used for the Stolen Lands region map in the Kingmaker module.
+   * @type {string}
+   */
+  static SCENE_ID = "AJ1k5II28u72JOmz";
+
+  /**
+   * The tool to toggle display of the info overlay (KingdomInfo).
+   * @type {SceneControlTool}
+   */
+  #infoTool;
+
+  /**
+   * The overlay for the kingdom's borders
+   * @type {KingdomInfoLayer}
+   */
+  kingdomInfoLayer;
+
+   /**
+   * Is the Kingmaker info map actively viewed on the canvas?
+   * @type {boolean}
+   */
+   get active() {
+    return kingmaker.region.active;
+  }
+
+  /* -------------------------------------------- */
+  /*  Canvas Lifecycle Hooks                      */
+  /* -------------------------------------------- */
+
+  /**
+   * Canvas configuration.
+   * @internal
+   */
+  _onConfig() {
+    if ( !this.active ) return;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Canvas initialization.
+   * @internal
+   */
+  _onInit() {
+    if ( !this.active ) return;
+    if ( canvas.visibilityOptions ) canvas.visibilityOptions.persistentVision = true;
+    this.kingdomInfoLayer = new KingdomInfoLayer();
+
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Canvas drawing.
+   * @internal
+   */
+  _onDraw() {
+    if ( !this.active ) return;
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Canvas ready.
+   * @internal
+   */
+  _onReady() {
+    if ( !this.active ) return;
+
+    canvas.grid.addChildAt(this.kingdomInfoLayer, canvas.grid.children.indexOf(canvas.grid.borders));
+    this.kingdomInfoLayer.draw();
+
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Canvas tear-down.
+   * @internal
+   */
+  _onTearDown() {
+    if ( !this.active ) return;
+
+  }
+
+  /* -------------------------------------------- */
+
+  /**
+   * Add special buttons to the scene controls palette when we are on the region map.
+   * @param buttons
+   * @internal
+   */
+  _extendSceneControlButtons(buttons) {
+    if ( canvas.id !== this.constructor.SCENE_ID ) return;
+    const tokens = buttons.find(b => b.name === "token");
+    this.#infoTool = {
+      name: "info",
+      title: "Toggle Kingdom Info",
+      icon: "fa-solid fa-circle-info",
+      visible: true,
+      toggle: true,
+      active: this.kingdomInfoLayer.visible ?? false,
+      onClick: () => this.kingdomInfoLayer.visible = !this.kingdomInfoLayer.visible
+    };
+    tokens.tools.push(this.#infoTool);
+  };
+}
---
+++ scripts/kingdom-map-enhancement.mjs
@@ -0,0 +1,41 @@
+import KingdomInfoMap from "./kingdom-info-map.mjs";
+
+/**
+ * The ID of the Kingmaker module
+ * @type {string}
+ */
+const MODULE_ID = "pf2e-kingmaker-map-enhancement";
+
+/* -------------------------------------------- */
+/*  Initialization                              */
+/* -------------------------------------------- */
+
+Hooks.once("init", function() {
+    /**
+     * Global reference to the Kingmaker module.
+     * @type {}
+     */
+    globalThis.kingdominfo = game.modules.get(MODULE_ID);
+});
+
+/* -------------------------------------------- */
+/*  Initialization                              */
+/* -------------------------------------------- */
+
+Hooks.once("setup", function() {
+    CONFIG.debug.hooks = true;
+    console.log("Kingdom | Initializing Kingdom Map Enhancement");
+    kingdominfo.kingdomInfoMap = new KingdomInfoMap();
+
+});
+
+/* -------------------------------------------- */
+/*  Canvas Events                               */
+/* -------------------------------------------- */
+
+Hooks.on("canvasConfig", () => kingdominfo.kingdomInfoMap._onConfig());
+Hooks.on("canvasInit", () => kingdominfo.kingdomInfoMap._onInit());
+Hooks.on("canvasDraw", () => kingdominfo.kingdomInfoMap._onDraw());
+Hooks.on("canvasReady", () => kingdominfo.kingdomInfoMap._onReady());
+Hooks.on("canvasTearDown", () => kingdominfo.kingdomInfoMap._onTearDown());
+Hooks.on("getSceneControlButtons", buttons => kingdominfo.kingdomInfoMap._extendSceneControlButtons(buttons));
